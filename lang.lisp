69960eddffd12b0d836fa7cd38a399e70867f0c5
Paul M. Rodriguez <pmr@ruricolist.com>
2017-01-19 14:40:27 -0600

    Handle a default export.

--- lang.lisp
+++ lang.lisp
@@ -18,19 +18,42 @@
                     if (and (consp form)
                             (eql (first form) :export))
                       collect form))
+            (export-default-form
+              (loop for form in body
+                    if (and (consp form) (eql (first form) :export-default))
+                      return form))
+            (meta-forms
+              (append export-forms (list export-default-form)))
+            (body
+              (remove-if (lambda (form)
+                           (member form meta-forms))
+                         body))
+
             (exports
               (loop for form in export-forms
                     append (rest form)))
-            (body
-              (remove-if (lambda (form)
-                           (member form export-forms))
-                         body)))
+            (default-export
+              (second export-default-form)))
+    (assert (not (and export-forms export-default-form)))
     `(progn
        ,@body
        (setq overlord/specials:*module*
-             ,(with-unique-names (ht)
-                `(let ((,ht (make-hash-table)))
-                   ,@(loop for var in exports
-                           for key = (alexandria:make-keyword var)
-                           collect `(setf (gethash ,key ,ht) ,var))
-                   ,ht))))))
+             ,(if export-default-form
+                  default-export
+                  (with-unique-names (ht)
+                    `(let ((,ht (make-hash-table)))
+                       ,@(loop for export in exports
+                               for (key form) = (etypecase export
+                                                  (cl:symbol
+                                                   (list (alexandria:make-keyword export)
+                                                         export))
+                                                  ((cl:cons (cl:eql function)
+                                                            (cl:cons symbol cl:null))
+                                                   (list (alexandria:make-keyword (second export))
+                                                         `(function ,(second export))))
+                                                  ((cl:cons (cl:eql macro-function)
+                                                            (cl:cons symbol cl:null))
+                                                   (list (alexandria:make-keyword (second export))
+                                                         `(macro-function ',(second export)))))
+                               collect `(setf (gethash ,key ,ht) ,form))
+                       ,ht)))))))
